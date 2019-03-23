5891e468775bc7ad7f986418582d428a24302b0c
David Silva <d.silvas@outlook.com>
2019-03-23 13:16:09 +0000

    Update categories

--- backend/resources/views/categories/create.blade.php
+++ backend/resources/views/categories/create.blade.php
@@ -4,7 +4,7 @@
 
 <div class="card card-default">
     <div class="card-header">
-        Create category
+        {{ isset($category) ? 'Edit Category' : 'Create Category' }}
     </div>
     <div class="card-body">
         @if($errors->any())
@@ -18,14 +18,28 @@
                 </ul>
             </div>
         @endif
-        <form action="{{ route('categories.store') }}" method="POST">
+        <form
+            action="{{ isset($category) ? route('categories.update', $category->id) : route('categories.store') }}"
+            method="POST"
+            >
             @csrf
+            @if(isset($category))
+                @method('PUT')
+            @endif
             <div class="form-group">
                 <label for="name">Name</label>
-                <input id="name" type="text" name="name" class="form-control">
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value="{{ isset($category) ? $category->name : '' }}"
+                    class="form-control"
+                    >
             </div>
             <div class="form-group">
-                <button class="btn btn-success">Add category</button>
+                <button class="btn btn-success">
+                    {{ isset($category) ? 'Update Category' :  'Add Category' }}
+                </button>
             </div>
         </form>
     </div>
