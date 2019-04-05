35a660170830620bb8c0942b4b3936a5e757aa41
David Silva <d.silvas@outlook.com>
2019-04-05 22:02:42 +0100

    Handle deletion of tags and categories

--- backend/app/Http/Controllers/TagsController.php
+++ backend/app/Http/Controllers/TagsController.php
@@ -94,6 +94,13 @@ class TagsController extends Controller
      */
     public function destroy(Tag $tag)
     {
+        if ($tag->posts->count() > 0) {
+            session()->flash(
+                'error',
+                'Tag cannot be deleted because it is associated to ' . $tag->posts->count() . ' posts'
+            );
+            return redirect()->back();
+        }
         $tag->delete();
 
         session()->flash('success', 'Tag deleted successfully');
