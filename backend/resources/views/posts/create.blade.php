59f01f0a5bcbb3ae1eb06e6c7026eae09b4460d3
David Silva <d.silvas@outlook.com>
2019-04-05 21:52:39 +0100

    Using select2 for tags

--- backend/resources/views/posts/create.blade.php
+++ backend/resources/views/posts/create.blade.php
@@ -122,14 +122,20 @@
 @section('css')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css">
 @endsection
 
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.1.0/trix.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 <script>
 flatpickr('#published_at', {
     enableTime: true
 })
+
+$(document).ready(function() {
+    $('#tags').select2();
+});
 </script>
 @endsection
