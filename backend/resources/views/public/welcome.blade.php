98e88ea2ec70ff0c98d8658a54f593eb81c26482
David Silva <d.silvas@outlook.com>
2019-08-02 17:20:04 +0100

    #23 - Fixed front page appearance on mobile

--- backend/resources/views/public/welcome.blade.php
+++ backend/resources/views/public/welcome.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-12 col-md-9">
+<div class="row mx-0">
+    <div class="col-12 col-md-9 px-0 px-md-1 px-lg-3">
         <h3>Latest posts</h3>
         <hr>
         @include('public.partials.posts-list', ['posts' => $posts])
     </div>
-
-    <div class="col-12 col-md-3">
+    
+    <div class="col-12 col-md-3 px-0 px-md-1 px-lg-3">
         @include('public.partials.sidebar')
     </div>
 </div>
